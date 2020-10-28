4155e9a2825aa0f0483e3cd7f80e4c9e8bce032f
jinsong cho <wlsthd3322@naver.com>
2020-10-28 23:09:43 +0900

    20.10.28
    hello python~

---
+++ prac_python/hello.py
@@ -0,0 +1,36 @@
+print('Hello, jungle')
+
+a = 3        # 3을 a에 넣는다
+print(a)
+b = a        # b에 3을 넣는다.
+print(b)
+a = 5        # a에 5라는 새로운 값을 넣는다.
+print(a, b)  # 5 3
+
+#사칙연산
+a = 7
+b = 2
+
+a+b   # 9
+a-b   # 5
+a*b   # 14
+a/b   # 3.5
+a//b  # 3 (몫)
+a%b   # 1 (나머지)
+a**b  # 49 (거듭제곱)
+
+a+3*b # 13 (여러 연산을 한 줄에 할 경우 사칙연산의 순서를 따른다.)
+
+
+a = 5
+a = a + 3 # 5에 3을 더한 값을 다시 a에 저장
+print(a)  # 8
+
+a += 1    # 줄여 쓸 수도 있다. 8에 1을 더해 9를 a에 저장. 사칙연산 다 가능하다.
+
+#문자열
+word1 = "jungle"
+word2 = 'coding'
+
+mac = "McDonald's"
+sentence = 'He said, "Hello!"'
